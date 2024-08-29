b7eed5cacb6694ac3c1eb4b308c34e84ed1a7bae
sephsx <olorbidajoseph05@gmail.com>
2024-08-29 20:53:48 +0800

    added daisyui for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+    ],
+
+    daisyui: {
+        themes: ["light", "dark", "cupcake","coffee","dim"],
+    },
+
+    plugins: [forms],
+};
